909dae18a9122c6352b6267156a968da9b176ebe
Ekaterina <ekaterinalarionova25@gmail.com>
2023-08-30 09:25:58 +0300

    added header

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <Header></Header>
+    <RouterView/>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+    components: {
+        Header
+    },
+    setup() {
+        // ...
+    }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="nav_wrap">
+        <ul class="nav_lists">
+            <li class="nav_list"><a href="#">Cписок лиг</a></li>
+            <li class="nav_list"><a href="#">Список команд</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+
+
+</style>
