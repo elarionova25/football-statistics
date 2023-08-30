452929e6869857e9c7116b7fce0f018074974116
Ekaterina <ekaterinalarionova25@gmail.com>
2023-08-30 11:24:50 +0300

    added config

--- src/resources/Football.js
+++ src/resources/Football.js
@@ -2,9 +2,20 @@ import axios from "axios";
 
 class FootballData {
     static async competitions() {
-        const {data} = await axios.get(`/api/v4/competitions`)
+        const {data} = await axios.get(`api/v4/competitions`, this.config);
+        return data;
+    }
+
+    static async leagues() {
+        const {data} = await axios.get(`api/v4/competitions/CL/matches`, this.config);
         return data;
     }
 }
+FootballData.token = '6783c28aa01c46119c51f90e43534c03'
+FootballData.config = {
+    headers: {
+        'X-Auth-Token': FootballData.token
+    }
+};
 
 export default FootballData;
